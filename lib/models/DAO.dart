98f2af52f5aa8d28ec0c1e5eb56bd69a81364bb9
drcoconutphd <66740352+drcoconutphd@users.noreply.github.com>
2022-05-16 01:34:41 +0800

    comment out fb again

--- lib/models/DAO.dart
+++ lib/models/DAO.dart
@@ -11,17 +11,17 @@ class DAO {
     count += 1;
   }
 
-  Future<Object> getValue() async {
-    final ref = FirebaseDatabase.instance.ref();
-    final snapshot = await ref.child('/').get();
-    if (snapshot.exists) {
-      // const int snapshot.value ;
-      return (snapshot.value);
-      // return 1;
-    } else {
-      // return -1;
-      print('No data available.');
-      // print('No data available.');
-    }
-  }
+  // Future<Object> getValue() async {
+  //   final ref = FirebaseDatabase.instance.ref();
+  //   final snapshot = await ref.child('/').get();
+  //   if (snapshot.exists) {
+  //     // const int snapshot.value ;
+  //     return (snapshot.value);
+  //     // return 1;
+  //   } else {
+  //     // return -1;
+  //     print('No data available.');
+  //     // print('No data available.');
+  //   }
+  // }
 }
