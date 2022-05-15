5a4c789062aa6100e9b37b42716287238e7e809f
rehohoho <ernie.birdy.ho@gmail.com>
2022-05-15 17:05:07 +0800

    Firebase: add dependencies for core and database

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:bushier2/views/home.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   // Fetch the available cameras before initializing the app.
   WidgetsFlutterBinding.ensureInitialized();
   final cameraDescList = await availableCameras();
   final firstCamera = cameraDescList.first;
+
+  // Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     MaterialApp(
